a94c0278adaf72f2750d6aabcb1c4433bf3afa06
AaronB22 <aaronthesnowboarder@live.com>
2026-01-19 23:21:32 -0800

    added more sprites per loop and a 2nd vector

--- src/main.cpp
+++ src/main.cpp
@@ -11,37 +11,45 @@ int main()
 {
     bn::core::init();
     bn::backdrop::set_color(bn::color(20, 20, 31));
-    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(20,50);
-    bn::sprite_ptr myCircle1 = bn::sprite_items::dot.create_sprite(40,50);
+    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(20, 50);
+    bn::sprite_ptr myCircle1 = bn::sprite_items::dot.create_sprite(40, 50);
 
-    bn::vector<bn::sprite_ptr, 10> circles={};
-     for(int x=-40; x<=40; x+=10){
-        BN_LOG("x value",x);
+    bn::vector<bn::sprite_ptr, 20> circles = {};
+    bn::vector<bn::sprite_ptr, 20> circles2 = {};
+    for (int x = -60; x <= 60; x += 10)
+    {
+        BN_LOG("x value", x);
         circles.push_back(bn::sprite_items::dot.create_sprite(x, 40));
-     }
+        circles2.push_back(bn::sprite_items::dot.create_sprite(x, 20));
+    }
+    int aRed = 31;
+    int aGreen = 10;
+    int aBlue = 20;
 
-    int aRed=31;
-    int aGreen=10;
-    int aBlue= 20;
-
-    int bRed=10;
-    int bGreen=20;
-    int bBlue= 31;
+    int bRed = 10;
+    int bGreen = 20;
+    int bBlue = 31;
 
     while (true)
     {
-        if(!bn::keypad::a_held() && !bn::keypad::b_held()){
-             bn::backdrop::set_color(bn::color(20, 20, 31));
-        }
-        if(bn::keypad::any_held() && bn::keypad::b_held()){
-            bn::backdrop::set_color(bn::color(aRed-bRed,bGreen-aGreen,bBlue-aBlue));
-        }
-        if (bn::keypad::a_held()&& !bn::keypad::b_held())
+        if (!bn::keypad::a_held() && !bn::keypad::b_held())
         {
+
+            bn::backdrop::set_color(bn::color(20, 20, 31));
+        }
+        if (bn::keypad::a_held() && bn::keypad::b_held())
+        {
+
+            bn::backdrop::set_color(bn::color(aRed - bRed, bGreen - aGreen, bBlue - aBlue));
+        }
+        if (bn::keypad::a_held() && !bn::keypad::b_held())
+        {
+
             bn::backdrop::set_color(bn::color(aRed, aGreen, aBlue));
         }
-        if (bn::keypad::b_held()&& !bn::keypad::a_held())
+        if (bn::keypad::b_held() && !bn::keypad::a_held())
         {
+
             bn::backdrop::set_color(bn::color(bRed, bGreen, bBlue));
         }
         bn::core::update();
